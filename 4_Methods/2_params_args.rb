6db6bef6fc754082e057a7dc82902d12a185d793
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-10-03 10:20:22 -0300

    more lessons

---
+++ 4_Methods/2_params_args.rb
@@ -0,0 +1,9 @@
+def praise_person(name)
+  puts "#{name} is amazing!"
+  puts "#{name} is charming!"
+  puts "#{name} is talented!"
+end
+
+praise_person("Ronaldo")
+puts
+praise_person("Flavielle")
